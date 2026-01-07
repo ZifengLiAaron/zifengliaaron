ecdd7b58e405b0e60dbb5fb99f25f8918f20a232
Zifeng Li <zifengl@vt.edu>
2026-01-06 21:26:14 -0500

    started adding nav bar

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './NavBar.css'
+import logo from '../../assets/Nazuna.png'
+
+const NavBar = () => {
+    return (
+        <nav className='container'>
+            <img src={logo} alt="Logo" className='logo' />
+            <ul>
+                <li>About Me</li>
+                <li>Education</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Work Experience</li>
+                <li><button className='btn'>Contact</button></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
